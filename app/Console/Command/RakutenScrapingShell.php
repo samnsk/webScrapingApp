eaa7f1388b53600fafb28c0436f84af8c1589614
samnsk <mas.shachiku@gmail.com>
2019-02-15 20:42:24 +0900

    [add] 楽天から商品を取得するクラスの雛形を作成

---
+++ app/Console/Command/RakutenScrapingShell.php
@@ -0,0 +1,79 @@
+<?php
+
+class RakutenScrapingShell extends AppShell
+{
+
+	public $uses = [
+		'Product'
+	];
+
+	/**
+	 * AmazonからJOYの商品情報を取得します
+	 * @throws Exception
+	 */
+	public function main()
+	{
+		$this->log('---------------------------------------------------------------', CRON_LOG);
+		$this->log('楽天の集計を始めます', CRON_LOG);
+		$html = file_get_contents(AMAZON_JOY);
+		$doc = phpQuery::newDocument($html);
+		try {
+			foreach ($doc->find('.s-result-item') as $item) {
+				$smallCategory = pq($item)->find('.s-item-container')->find('.a-spacing-mini')->find('.a-spacing-none')->eq(1)->text();
+				if ($smallCategory === 'ジョイ') {
+
+					// 取得済みの商品は保存しない
+					$productCode = pq($item)->attr('data-asin');
+					if ($this->__productCodeExist($productCode)) {
+						continue;
+					}
+					$this->log(' 楽天: '.'商品コード'.$productCode.'の商品を登録しました', CRON_LOG);
+
+					$productName = pq($item)->find('h2')->text();
+					$productImageUrl = pq($item)->find('.a-column')->find('img')->attr('src');
+					$productUrl = pq($item)->find('.s-item-container')->find('.a-spacing-mini')->find('.a-spacing-none')->find('.a-link-normal')->attr('href');
+					$productPrice = pq($item)->find('.s-item-container')->find('.a-color-price')->text();
+					$productPrice = trim(str_replace(['￥', ','], '', $productPrice));
+
+					$productData = [
+						'Product' => [
+							'name' => $productName,
+							'code' => $productCode,
+							'image_url' => $productImageUrl,
+							'big_category' => DAILY_USE_ITEM,
+							'small_category' => $smallCategory,
+							'url' => $productUrl,
+							'description' => null,
+							'price' => $productPrice
+						],
+					];
+
+					$this->Product->create();
+					$this->Product->save($productData);
+				}
+			}
+		} catch (Exception $e) {
+			$this->log('楽天の集計中にエラーが発生しました[エラー内容:'.$e->getMessage().']', CRON_LOG);
+			throw $e;
+		}
+		$this->log('楽天の集計を終了します', CRON_LOG);
+		$this->log('---------------------------------------------------------------', CRON_LOG);
+	}
+
+	/**
+	 * 同一の商品コードがDBに存在するかチェック
+	 *
+	 * @param $productCode
+	 * @return bool
+	 */
+	private function __productCodeExist($productCode)
+	{
+		$productCodeList = $this->Product->find('all', [
+			'fields' => 'code'
+		]);
+		$productCodeList = Hash::extract($productCodeList, '{n}.Product.code');
+
+		return in_array($productCode, $productCodeList);
+	}
+
+}
